c854676e804b247b46e792651eb5e7f9640fce41
robbee <alexandre.bonte@beesearch.fr>
2015-05-31 23:38:03 +0200

    tension histo

--- www/views/phonegap/device.js
+++ www/views/phonegap/device.js
@@ -1,7 +1,47 @@
 angular.module(_CONTROLLERS_).controller('device', function($scope, $ionicPlatform) {
     console.log('### device controller in');
     
-    //$scope.device = $ionicPlatform.device();
+
+   	$scope.top5ProductsChart = {
+        title: {
+            text: 'Monthly Average Tension',
+            x: -20 //center
+        },
+        subtitle: {
+            text: 'Source: ensemble.io',
+            x: -20
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Tension (°P/m)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'Haut',
+            data: [150, 150, 145, 145, 140, 140, 142, 140, 140, 135, 130, 120]
+        }, {
+            name: 'Haut',
+            data: [140, 140, 138, 135, 133, 130, 120, 120, 120, 114, 110, 110]
+        }]
+	}
 
     console.log('### device controller out');
 });
