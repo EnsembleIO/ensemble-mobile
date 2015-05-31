9fe2577fa20067e71cb7532e30ad5f98e9e1df25
robbee <alexandre.bonte@beesearch.fr>
2015-05-31 22:14:18 +0200

    search stuff

---
+++ www/views/details/call.js
@@ -0,0 +1,8 @@
+angular.module(_CONTROLLERS_).controller('call', function($scope, BACKEND_PROTOCOL, BACKEND_HOST, BACKEND_PORT) {
+    console.log('### call controller in');
+
+    $scope.url= BACKEND_PROTOCOL + '://' + BACKEND_HOST + ':' + BACKEND_PORT;
+    console.log('url : ' + $scope.url);
+    
+    console.log('### call controller out');
+});
