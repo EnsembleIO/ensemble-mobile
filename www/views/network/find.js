a30959261c932c57731e2c6b14b8f7c52daf9de2
robbee <alexandre.bonte@beesearch.fr>
2015-06-04 14:02:25 +0200

    map ok

--- www/views/network/find.js
+++ www/views/network/find.js
@@ -1,8 +1,6 @@
 angular.module(_CONTROLLERS_).controller('find', function($scope, $ionicLoading, $compile, BACKEND_PROTOCOL, BACKEND_HOST, BACKEND_PORT) {
     console.log('### find controller in');
 
-
-
     $scope.map = { center: { latitude: 47.205129, longitude: -1.576258 }, zoom: 15 };
 
     $scope.options = {scrollwheel: false};
@@ -32,30 +30,30 @@ angular.module(_CONTROLLERS_).controller('find', function($scope, $ionicLoading,
               control: {}
           }];
 
-          $scope.marker = {
-            id: 0,
-            coords: {
-                  latitude: 47.205429,
-                  longitude: -1.579058
-            },
-            options: { draggable: true },
-            events: {
-              dragend: function (marker, eventName, args) {
-                $log.log('marker dragend');
-                var lat = marker.getPosition().lat();
-                var lon = marker.getPosition().lng();
-                $log.log(lat);
-                $log.log(lon);
+    $scope.marker = {
+      id: 0,
+      coords: {
+            latitude: 47.205429,
+            longitude: -1.579058
+      },
+      options: { draggable: true },
+      events: {
+        dragend: function (marker, eventName, args) {
+          $log.log('marker dragend');
+          var lat = marker.getPosition().lat();
+          var lon = marker.getPosition().lng();
+          $log.log(lat);
+          $log.log(lon);
 
-                $scope.marker.options = {
-                  draggable: true,
-                  labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-                  labelAnchor: "100 0",
-                  labelClass: "marker-labels"
-                };
-              }
-            }
+          $scope.marker.options = {
+            draggable: true,
+            labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+            labelAnchor: "100 0",
+            labelClass: "marker-labels"
           };
+        }
+      }
+    };
 
     console.log('### find controller out');
 });
